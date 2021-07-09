23d3462645cfac339c1dd15e10d4f4a31f86f0dd
Vicente Nicolás De la Rosa Sánchez <vicente.delarosa@sansano.usm.cl>
2021-07-09 00:19:56 -0400

    Añadido metodo GET prints realizados

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ const app = express();
 
 const axios = require('axios');
 
-app.get("/", async (req, res) =>{
-    try {
-        const response = await axios.get('https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/audit-report');
-        const data = response.data;
-        console.log(data);
-        return res.send(data);
-    } catch(error){
-        res.status(400).send(error);
-    }
-});
+app.use(express.json());
+app.use("/prints-count", require("./routes/printsCount"));
 
 //servidor
 app.listen(8080, () => {console.log("El servidor está funcionando")});
